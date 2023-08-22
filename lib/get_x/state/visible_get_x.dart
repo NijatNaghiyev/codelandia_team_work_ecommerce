38ea9c59c34a421f4ebdb6f4343ce5bda001d3bb
Nijat Naghiyev <nicat614@icloud.com>
2023-08-22 18:39:17 +0400

    showDialog

---
+++ lib/get_x/state/visible_get_x.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class Visible extends GetxController {
+  RxBool isVisible = false.obs;
+}
--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:codelandia_team_work_ecommerce/get_x/state/visible_get_x.dart';
 import 'package:codelandia_team_work_ecommerce/hive/cart_list_hive.dart';
 import 'package:codelandia_team_work_ecommerce/screens/main_screen.dart';
 import 'package:flutter/material.dart';
@@ -12,13 +13,15 @@ import '../generated/assets.dart';
 
 class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key, required this.totalPrice});
+
   final int totalPrice;
+
   @override
   State<OrderScreen> createState() => _OrderScreenState();
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  bool isVisible = false;
+  Visible visibleController = Get.put(Visible());
   final fullName = TextEditingController();
   final phone = TextEditingController();
   final address = TextEditingController();
@@ -27,6 +30,7 @@ class _OrderScreenState extends State<OrderScreen> {
   List<String> cities = ["Bakı", "Sumqayıt", "Gəncə"];
   int currentStep = 0;
   String selectedCreditKart = "Bir bank";
+
   List<Step> getSteps() => [
         Step(
           state: currentStep > 0 ? StepState.complete : StepState.indexed,
@@ -43,7 +47,7 @@ class _OrderScreenState extends State<OrderScreen> {
               controller: phone,
               maxLength: 9,
               decoration: InputDecoration(
-                prefix: Text("+994 "),
+                prefix: const Text("+994 "),
                 label: Text("Telefon".tr),
               ),
               keyboardType: TextInputType.number,
@@ -211,7 +215,7 @@ class _OrderScreenState extends State<OrderScreen> {
           isActive: currentStep >= 3,
           title: Text("Təsdiq".tr),
           content: Card(
-            shape: RoundedRectangleBorder(
+            shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(
                 Radius.circular(16),
               ),
@@ -226,10 +230,11 @@ class _OrderScreenState extends State<OrderScreen> {
                       children: [
                         TextSpan(
                           text: '${'Ad, soyad'.tr}: ',
-                          style: TextStyle(color: Colors.teal, fontSize: 20),
+                          style:
+                              const TextStyle(color: Colors.teal, fontSize: 20),
                         ),
                         TextSpan(
-                          text: '${fullName.text}',
+                          text: fullName.text,
                           style: TextStyle(
                             fontSize: 20,
                             color: Get.isDarkMode ? Colors.white : Colors.black,
@@ -243,7 +248,8 @@ class _OrderScreenState extends State<OrderScreen> {
                       children: [
                         TextSpan(
                           text: '${'Telefon'.tr}: ',
-                          style: TextStyle(color: Colors.teal, fontSize: 20),
+                          style:
+                              const TextStyle(color: Colors.teal, fontSize: 20),
                         ),
                         TextSpan(
                           text: '+994${phone.text}',
@@ -260,10 +266,11 @@ class _OrderScreenState extends State<OrderScreen> {
                       children: [
                         TextSpan(
                           text: '${'City'.tr}: ',
-                          style: TextStyle(color: Colors.teal, fontSize: 20),
+                          style:
+                              const TextStyle(color: Colors.teal, fontSize: 20),
                         ),
                         TextSpan(
-                          text: ' ${city}',
+                          text: ' $city',
                           style: TextStyle(
                             fontSize: 20,
                             color: Get.isDarkMode ? Colors.white : Colors.black,
@@ -271,7 +278,8 @@ class _OrderScreenState extends State<OrderScreen> {
                         ),
                         TextSpan(
                           text: ' ${'Street'.tr}: ',
-                          style: TextStyle(color: Colors.teal, fontSize: 20),
+                          style:
+                              const TextStyle(color: Colors.teal, fontSize: 20),
                         ),
                         TextSpan(
                           text: ' ${address.text}',
@@ -288,11 +296,12 @@ class _OrderScreenState extends State<OrderScreen> {
                       children: [
                         TextSpan(
                           text: '${'Ödəniş'.tr}: ',
-                          style: TextStyle(color: Colors.teal, fontSize: 20),
+                          style:
+                              const TextStyle(color: Colors.teal, fontSize: 20),
                         ),
                         TextSpan(
                           text:
-                              '${payType == "credit" ? selectedCreditKart : payType}',
+                              payType == "credit" ? selectedCreditKart : payType,
                           style: TextStyle(
                             fontSize: 20,
                             color: Get.isDarkMode ? Colors.white : Colors.black,
@@ -306,7 +315,8 @@ class _OrderScreenState extends State<OrderScreen> {
                       children: [
                         TextSpan(
                           text: '${'Toplam məbləğ'.tr}: ',
-                          style: TextStyle(color: Colors.teal, fontSize: 20),
+                          style:
+                              const TextStyle(color: Colors.teal, fontSize: 20),
                         ),
                         TextSpan(
                           text: '\$ ${widget.totalPrice}',
@@ -328,127 +338,134 @@ class _OrderScreenState extends State<OrderScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            "Rəsmiləşdirilmə".tr,
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-          ),
-          backgroundColor: Colors.teal,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Rəsmiləşdirilmə".tr,
+          style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
         ),
-        body: Theme(
-          data: Theme.of(context).copyWith(
-              colorScheme: const ColorScheme.light(primary: Colors.teal)),
-          child: Stepper(
-            steps: getSteps(),
-            currentStep: currentStep,
-            onStepContinue: () {
-              if ((currentStep == 0 &&
-                      (fullName.text.isEmpty || phone.text.length < 9)) ||
-                  (currentStep == 1 &&
-                      (city == null || address.text.isEmpty))) {
-                ScaffoldMessenger.of(context).clearSnackBars();
-                showTopSnackBar(
-                  Overlay.of(context),
-                  CustomSnackBar.error(
-                    message: 'All fields must be filled'.tr,
-                  ),
-                );
-              } else {
-                currentStep != getSteps().length - 1
-                    ? setState(() {
-                        currentStep++;
-                      })
-                    : showDialog(
-                        context: context,
-                        builder: (context) {
-                          Timer(
-                            Duration(seconds: 2),
-                            () {
-                              setState(() {
-                                isVisible = true;
-                              });
-                            },
-                          );
-                          return Card(
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Lottie.asset(
-                                      repeat: false,
-                                      'assets/lottie/lottie_validation.json'),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  OutlinedButton(
-                                    onPressed: () {
-                                      isVisible = false;
-                                      cartListController.cartList.clear();
-                                      Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const MainScreen(),
-                                        ),
-                                      );
-                                    },
-                                    child: Text(
-                                      'Əsas səhifəyə qayıt'.tr,
-                                      style: TextStyle(
-                                        color: Get.isDarkMode
-                                            ? Colors.white
-                                            : Colors.teal,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      );
-              }
-            },
-            onStepCancel: () {
-              currentStep == 0
-                  ? null
-                  : setState(() {
-                      currentStep--;
-                    });
-            },
-            controlsBuilder: (context, details) {
-              return Container(
-                margin: const EdgeInsets.only(top: 50),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
-                          onPressed: details.onStepContinue,
-                          child: Text(currentStep == getSteps().length - 1
-                              ? "Təsdiq".tr
-                              : "Növbəti".tr)),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    if (currentStep != 0)
-                      Expanded(
-                        child: ElevatedButton(
-                            onPressed: details.onStepCancel,
-                            child: Text("Geriyə".tr)),
-                      ),
-                  ],
+        backgroundColor: Colors.teal,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Theme(
+        data: Theme.of(context).copyWith(
+            colorScheme: const ColorScheme.light(primary: Colors.teal)),
+        child: Stepper(
+          steps: getSteps(),
+          currentStep: currentStep,
+          onStepContinue: () {
+            if ((currentStep == 0 &&
+                    (fullName.text.isEmpty || phone.text.length < 9)) ||
+                (currentStep == 1 && (city == null || address.text.isEmpty))) {
+              ScaffoldMessenger.of(context).clearSnackBars();
+              showTopSnackBar(
+                Overlay.of(context),
+                CustomSnackBar.error(
+                  message: 'All fields must be filled'.tr,
                 ),
               );
-            },
-          ),
-        ));
+            } else {
+              currentStep != getSteps().length - 1
+                  ? setState(() {
+                      currentStep++;
+                    })
+                  : showDialog(
+                      context: context,
+                      builder: (context) {
+                        Timer(
+                          const Duration(seconds: 4),
+                          () {
+                            setState(() {});
+                            visibleController.isVisible.value = true;
+                          },
+                        );
+                        return Card(
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Lottie.asset(
+                                    repeat: false,
+                                    'assets/lottie/lottie_validation.json'),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                Obx(() {
+                                  return Visibility(
+                                    visible: visibleController.isVisible.value,
+                                    child: OutlinedButton(
+                                      onPressed: () {
+                                        visibleController.isVisible.value =
+                                            false;
+
+                                        cartListController.cartList.clear();
+                                        Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                const MainScreen(),
+                                          ),
+                                        );
+                                      },
+                                      child: Text(
+                                        'Əsas səhifəyə qayıt'.tr,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          color: Get.isDarkMode
+                                              ? Colors.white
+                                              : Colors.teal,
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }),
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+            }
+          },
+          onStepCancel: () {
+            currentStep == 0
+                ? null
+                : setState(() {
+                    currentStep--;
+                  });
+          },
+          controlsBuilder: (context, details) {
+            return Container(
+              margin: const EdgeInsets.only(top: 50),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                        onPressed: details.onStepContinue,
+                        child: Text(currentStep == getSteps().length - 1
+                            ? "Təsdiq".tr
+                            : "Növbəti".tr)),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  if (currentStep != 0)
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: details.onStepCancel,
+                        child: Text("Geriyə".tr),
+                      ),
+                    ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
